b12623008a99957d463c2f3646928a467fadd2ff
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-03-09 17:46:19 +0100

    Add preupdating and cleaning

--- bootstrap_ubuntu.sh
+++ bootstrap_ubuntu.sh
@@ -22,9 +22,26 @@ mkdir -p "/tmp/$1"
 echo "Bootstrapping Ubuntu $1 $2"
 debootstrap --components=main,restricted,universe,multiverse "$2" /tmp/"$1" http://archive.ubuntu.com/ubuntu
 
+echo "Updating all packages inside rootfs"
+chroot /tmp/"$1" /bin/bash -c "apt update -y && apt upgrade -y"
+
+echo "Cleaning rootfs"
+# Remove unneeded/temporary files to reduce the rootfs size
+rm -rf /tmp/"$1"/boot/*
+rm -rf /tmp/"$1"/dev/*
+rm -rf /tmp/"$1"/lost+found/*
+rm -rf /tmp/"$1"/media/*
+rm -rf /tmp/"$1"/mnt/*
+rm -rf /tmp/"$1"/proc/*
+rm -rf /tmp/"$1"/run/*
+rm -rf /tmp/"$1"/sys/*
+rm -rf /tmp/"$1"/tmp/*
+rm -rf /tmp/"$1"/var/tmp
+rm -rf /tmp/"$1"/var/cache
+
 echo "Compressing rootfs"
 cd "/tmp/$1"
 tar -cv -I 'xz -9 -T0' -f ../ubuntu-rootfs-"$1".tar.xz ./
 
 echo "Calculating sha256sum"
-sha256sum ../ubuntu-rootfs-"$1".tar.xz > ../ubuntu-rootfs-"$1".sha256
+sha256sum ../ubuntu-rootfs-"$1".tar.xz >../ubuntu-rootfs-"$1".sha256
